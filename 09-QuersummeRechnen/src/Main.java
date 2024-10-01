21bbcf07c98b6325417f93ccb78708bd59785336
Jana <jana@slavnic.com>
2024-10-01 14:55:54 +0200

    aufgabe 9 gemacht

--- 09-QuersummeRechnen/src/Main.java
+++ 09-QuersummeRechnen/src/Main.java
@@ -7,14 +7,21 @@ public class Main {
         System.out.println("Bitte geben Sie eine positive Ganzzahl ein: ");
         int zahl = scanner.nextInt();
 
-        int quersumme = 0;
+        int quersumme = berechneQuersumme(zahl);
+
+
+        //Ausgabe Quersumme
+        System.out.println("Die Quersumme ist " +quersumme);
+    }
+
+    private static int berechneQuersumme(int zahl) {
+        int sum = 0;
 
         //Berechnung der Quersumme
-      while (zahl>0){
-          quersumme += zahl%10;//letze zahl zu quersumme machen
-          zahl /=10;//zahl um 10 verkleinern
-      }
-      //Ausgabe Quersumme
-        System.out.println("Die Quersumme ist " +quersumme);
+        while (zahl > 0){
+            sum = sum + zahl % 10;//letze zahl zu quersumme machen
+            zahl = zahl / 10;//zahl um 10 verkleinern
+        }
+        return sum;
     }
 }
