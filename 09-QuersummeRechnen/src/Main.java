8f4c96fe626e4e6568326e4af4a4c6ad7847a3da
Jana <jana@slavnic.com>
2024-10-01 13:46:55 +0200

    aufgabe 9 gemacht

--- 09-QuersummeRechnen/src/Main.java
+++ 09-QuersummeRechnen/src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        //test
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.println("Bitte geben Sie eine positive Ganzzahl ein: ");
+        int zahl = scanner.nextInt();
+
+        int quersumme = 0;
+
+        //Berechnung der Quersumme
+      while (zahl>0){
+          quersumme += zahl%10;//letze zahl zu quersumme machen
+          zahl /=10;//zahl um 10 verkleinern
+      }
+      //Ausgabe Quersumme
+        System.out.println("Die Quersumme ist " +quersumme);
     }
 }
